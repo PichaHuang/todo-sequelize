9cbe7f74bd1fd48993e0fb9170c9f0734214ce63
PichaHuang <ychuang33@gmail.com>
2022-04-14 16:59:33 +0800

    complete project with mySQL.

---
+++ routes/modules/home.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+
+const db = require('../../models')
+const Todo = db.Todo
+
+router.get('/', (req, res) => {
+  const userId = req.user.id
+
+  return Todo.findAll({
+    where: { userId },
+    order: [['id', 'ASC']],
+    raw: true,
+    nest: true
+  })
+    .then((todos) => { return res.render('index', { todos: todos }) })
+    .catch((error) => { return res.status(422).json(error) })
+})
+
+module.exports = router
